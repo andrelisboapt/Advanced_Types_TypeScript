0a86601bf4af4bff9d767445e66f4318c3fbeccc
André Lisboa <andrelisboapt@hotmail.com>
2023-06-09 18:07:43 +0100

    intersection types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
-// Code goes here!
+type Admin = { //here we are using type but could be an interface too
+    name: string;
+    privileges: string[];
+};
+
+type Employee = {
+    name: string;
+    startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee; //this type needs to include all the properties from both types
+// interface ElevatedEmployee extends Employee, Admin {}
+
+
+
+const e1: ElevatedEmployee = {
+    name: 'Max',
+    privileges: ['create-server'],
+    startDate: new Date() //current time
+};
+
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric; //this means that needs to be a number since both types/interface have that type in common
+
+
+
+
+
