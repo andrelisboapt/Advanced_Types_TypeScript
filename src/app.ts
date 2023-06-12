d9c199253d4c2061fc472238e79910f159f35a8b
André Lisboa <andrelisboapt@hotmail.com>
2023-06-12 17:30:26 +0100

    Type Guards

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,55 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric; //this means that needs to be a number since both types/interface have that type in common
 
+function add(a: Combinable, b: Combinable){
+    if (typeof a === 'string' || typeof b === 'string'){ //this is a type guard, because we have flexibility on the type but this make sures that we can compile, otherwise we could get errors (for example sum of a string with a number)
+        return a.toString()+ b.toString();
+    }
+    return a + b;
+}
 
+type UnknownEmployee = Employee | Admin;
 
+function printEmployeeInformation(emp: UnknownEmployee){
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp){
+        console.log('Privileges: ' + emp.privileges)
+    }
+    if ('startDate' in emp){
+        console.log('Start Date: ' + emp.startDate)
+    }
+}
 
+printEmployeeInformation(e1);
 
+class Car {
+    drive(){
+        console.log('Driving...')
+    }
+}
+
+class Truck {
+    drive(){
+        console.log('Driving a truck...')
+    }
+
+    loadCargo(amount: number){
+        console.log('Loading cargo...' + amount)
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle){
+    vehicle.drive();
+    //if ('loadCargo' in vehicle){ //we could use this too
+    if(vehicle instanceof Truck){ //type guard is the idea of confirm that the object or property exists before we run
+        vehicle.loadCargo(1000);
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
